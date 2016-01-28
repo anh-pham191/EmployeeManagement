68b3c316a94528306cd52221c23518cc0891a00d
anhpt <tuananh191194@gmail.com>
2016-01-28 14:53:01 +0700

    Add CRUD function

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
 use Intervention\Image\Facades\Image;
 
 class MembersController extends Controller
@@ -39,11 +40,49 @@ class MembersController extends Controller
             Image::make($image->getRealPath())->resize(intval($size), null, function($contstraint){
                 $contstraint->aspectRatio();
             })->save($path);
-            $member->image = 'public/img/' . $filename;
+            $member->image = 'img/' . $filename;
         }
 
         $member->save();
         return redirect('member');
 
+    }
+
+    public function show($id){
+        $member = Member::find($id);
+        return view('member.show', compact('member'));
+    }
+
+    public function edit($id){
+        $member = Member::find($id);
+        return view('member.edit', compact('member'));
+    }
+
+    public function update($id, MemberRequest $request){
+        $member = Member::find($id);
+        $input = Input::all();
+        $member->name = $input['name'];
+        $member->email = $input['email'];
+        $member->phone = $input['phone'];
+        if ($request->file()) {
+            $image = $request->file('image');
+            $filename = $request->file('image')->getClientOriginalName();
+            $path = public_path('img/' . $filename);
+
+            $size = '200,200';
+            Image::make($image->getRealPath())->resize(intval($size), null, function($contstraint){
+                $contstraint->aspectRatio();
+            })->save($path);
+            $member->image = 'img/' . $filename;
+        }
+
+        $member->save();
+        return redirect('member');
+    }
+
+    public function destroy($id){
+        $member = Member::find($id);
+        $member->delete();
+        return redirect('member');
     }
 }
